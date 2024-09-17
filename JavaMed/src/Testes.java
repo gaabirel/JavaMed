08fcfe273279db1670700b5b36f0d29f5d36de7e
gaabirel <gabrieljogos400@gmail.com>
2024-09-17 15:52:24 -0300

    model testado e sem erros

---
+++ JavaMed/src/Testes.java
@@ -0,0 +1,46 @@
+import model.*;
+import view.*;
+import controller.*;
+import dao.*;
+
+/*
+ * TESTES 
+ *      Model       [X] 
+ *      Controller  []
+ *      View        []
+ *      DAO         []
+*/
+
+/* MODEL
+
+Admin admin = new Admin(1, "987654321", "12312312312", "admin", "admin");
+Paciente paciente = new Paciente(1, "984050860", "45645645645", "paciente", "soupaciente123");
+Medico medico = new Medico(1, "987654321", "78978978978", "medico", "soumedico123");
+Atendente atendente = new Atendente(1, "987654321", "98798798798", "atendente", "souatendente123");
+
+Usuario[] usuarios = {admin, paciente, medico, atendente};
+
+// printar todos os tipos de usuarios instanciados
+for ( Usuario usuario : usuarios ) {
+    System.out.println(usuario.toString());
+}
+
+*/
+
+/* DAO
+*/
+
+/* VIEW
+*/
+
+/* CONTROLLER
+*/
+
+public class Testes {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
