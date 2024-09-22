2358bbbf8faf11c2b0fbb2199edec602bac424a1
gaabirel <gabrieljogos400@gmail.com>
2024-09-22 10:57:36 -0300

    ConsultaDAO atualizado com metodos de verificacao de IDS e stringToConsulta

--- JavaMed/src/dao/ConsultaDAO.java
+++ JavaMed/src/dao/ConsultaDAO.java
@@ -31,7 +31,7 @@ public class ConsultaDAO {
     }
 
     /*
-     * Método que salva uma único consulta no arquivo
+     * Método que salva uma única consulta no arquivo
     */
     public void salvarUnico(Consulta consulta) throws IOException{
 
@@ -55,7 +55,7 @@ public class ConsultaDAO {
 
         BufferedWriter bfw = new BufferedWriter(new FileWriter(nomeArquivo, true));
 
-        for (Consulta consulta : consultas) {
+        for ( Consulta consulta : consultas ) {
             // verificando se a consulta nao existe
             if ( !verificarConsultaId(consulta.getIdConsulta()) ){
                 bfw.write(consulta.toString() + "\n");
@@ -78,22 +78,101 @@ public class ConsultaDAO {
 
         while( ( linha = bfr.readLine() ) != null ) {
 
-            // idPaciente;idMedico;data;diagnostico
-            String[] partes = linha.split(";");
-            int idCon = Integer.parseInt(partes[0]);
-            int idPac = Integer.parseInt(partes[1]);
-            int idMed = Integer.parseInt(partes[2]);
-            String data = partes[3];
-            String diagnostico = partes[4];
+            consultas.add(stringToConsulta(linha));
 
-            consultas.add(new Consulta(idCon, idPac, idMed, data, diagnostico));
         }
 
         bfr.close();
         return consultas;
 
     }
+
+    /*
+     * Método que carrega uma consulta, recebendo o ID da consulta
+    */
+    public Consulta carregaConsultaById(int idBus) throws IOException {
+
+        // verificando se a consulta existe
+        if ( !verificarConsultaId(idBus) ) return null;
+
+        BufferedReader bfr = new BufferedReader(new FileReader(nomeArquivo));
+
+        String linha;
+
+        while ( (linha = bfr.readLine()) != null ) {
+
+            Consulta consulta = stringToConsulta(linha);
+
+            if ( consulta.getIdConsulta() == idBus ) {
+                bfr.close();
+                return consulta;
+            }
+
+        }
+
+        bfr.close();
+        return null;
+
+    }
+
+    /*
+     * Método que retorna as consultas com base no ID do paciente
+    */
+    public List<Consulta> getConsultasByIdPaciente(int idPac) throws IOException {
+
+        // verificando se existe alguma ocorrencia do ID do paciente, caso nao exista retorna null
+        if ( !verificaPacienteId(idPac) ) return null;
+
+        List<Consulta> consultas = new ArrayList<>();
+        BufferedReader bfr = new BufferedReader(new FileReader(nomeArquivo));
+
+        String linha;
+
+        while ( (linha = bfr.readLine()) != null ) {
     
+            Consulta consulta = stringToConsulta(linha);
+
+            if ( consulta.getPaciente() == idPac ) {
+                consultas.add(consulta);
+            }
+
+        }
+
+        bfr.close();
+        return consultas;
+    }
+
+    /*
+     * Método que retorna as consultas com base no ID do medico, caso nao exista retorna null
+    */
+    public List<Consulta> getConsultasByIdMedico(int idMed) throws IOException {
+
+        // verificando se existe alguma ocorrencia do ID do medico
+        if ( !verificaMedicoId(idMed) ) return null;
+
+        List<Consulta> consultas = new ArrayList<>();
+        BufferedReader bfr = new BufferedReader(new FileReader(nomeArquivo));
+
+        String linha;
+
+        while ( (linha = bfr.readLine()) != null ) {
+    
+            Consulta consulta = stringToConsulta(linha);
+
+            if ( consulta.getMedico() == idMed ) {
+                consultas.add(consulta);
+            }
+
+        }
+
+        bfr.close();
+        return consultas;
+
+    }
+
+    /*
+     * Método que exclui uma única consulta, recebendo um ID
+    */
     public void excluirUnico(int idExc) throws IOException {
 
         // verificando se o usuario existe
@@ -109,18 +188,11 @@ public class ConsultaDAO {
 
         while ( (linha = bfr.readLine()) != null ) {
 
-            String[] partes = linha.split(";");
-            // id;telefone;cpf;nome;senha
-            int id = Integer.parseInt(partes[0]);
-            int idPaciente = Integer.parseInt(partes[1]);
-            int idMedico = Integer.parseInt(partes[2]);
-            String data = partes[3];
-            String diagnostico = partes[4];
+            Consulta consulta = stringToConsulta(linha);
 
-            // caso não seja o usuario a ser excluido
-            if ( id != idExc ) {
-                // adiciono no novo array
-                consultasAtualizadas.add(new Consulta(id, idPaciente, idMedico, data, diagnostico));
+            // caso não seja o usuario a ser excluido, adiciona no novo array
+            if ( consulta.getIdConsulta() != idExc ) {
+                consultasAtualizadas.add(consulta);
             }
 
         }
@@ -129,7 +201,7 @@ public class ConsultaDAO {
 
         BufferedWriter bfw = new BufferedWriter(new FileWriter(nomeArquivo));
 
-        // salvo esse novo array no arquivo
+        // salvar esse novo array no arquivo
         for ( Consulta consultas : consultasAtualizadas ) {
             bfw.write(consultas.toString() + "\n");
         }
@@ -138,7 +210,7 @@ public class ConsultaDAO {
     }
 
     /*
-     *  Método que exclui todos os usuarios
+     *  Método que exclui todas as consultas
     */
     public void excluirTodos() throws IOException {
 
@@ -151,7 +223,7 @@ public class ConsultaDAO {
     }
     
     /*
-     * Método que verifica se determinada consulta já está no banco com base nos IDs do paciente e do médico
+     * Método que verifica se determinada consulta já está no banco com base no ID da consulta
     */
     public Boolean verificarConsultaId(int idConsulta) throws IOException {
 
@@ -160,9 +232,10 @@ public class ConsultaDAO {
         String linha;
 
         while ( (linha = bfr.readLine()) != null ) {
-            String[] partes = linha.split(";");
-            int id = Integer.parseInt(partes[0]);
-            if (id == idConsulta) {
+
+            Consulta consulta = stringToConsulta(linha);
+
+            if (consulta.getIdConsulta() == idConsulta) {
                 bfr.close();
                 return true;
             }
@@ -170,6 +243,66 @@ public class ConsultaDAO {
 
         bfr.close();
         return false;
+
+    }
+
+    /*
+     * Método que verifica se o paciente tem alguma consulta, recebendo o ID do paciente
+    */
+    public Boolean verificaPacienteId(int idPac) throws IOException {
+        BufferedReader bfr = new BufferedReader(new FileReader(nomeArquivo));
+
+        String linha;
+
+        while ( (linha = bfr.readLine()) != null ) {
+
+            Consulta consulta = stringToConsulta(linha);
+
+            if (consulta.getPaciente() == idPac) {
+                bfr.close();
+                return true;
+            }
+        }
+
+        bfr.close();
+        return false;
+    }
+    /*
+     * Método que verifica se o medico tem alguma consulta, recebendo o ID do medico
+    */
+    public Boolean verificaMedicoId(int idMed) throws IOException {
+        BufferedReader bfr = new BufferedReader(new FileReader(nomeArquivo));
+
+        String linha;
+
+        while ( (linha = bfr.readLine()) != null ) {
+
+            Consulta consulta = stringToConsulta(linha);
+
+            if (consulta.getMedico() == idMed) {
+                bfr.close();
+                return true;
+            }
+        }
+
+        bfr.close();
+        return false;
+    }
+
+    /*
+     * Método que recebe uma string padronizada e retorna uma consulta
+    */
+    public Consulta stringToConsulta(String consulta) {
+
+        // idConsulta;idPaciente;idMedico;data;diagnostico 
+        String[] partes = consulta.split(";");
+        int idCon = Integer.parseInt(partes[0]);
+        int idPac = Integer.parseInt(partes[1]);
+        int idMed = Integer.parseInt(partes[2]);
+        String data = partes[3];
+        String diag = partes[4];
+
+        return new Consulta(idCon, idPac, idMed, data, diag);
 
     }
 
