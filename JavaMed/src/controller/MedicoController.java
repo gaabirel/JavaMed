6ebec33775abe584d002da57c953c6240a78ffc6
Tiago Mamede <113864916+tiagomamede@users.noreply.github.com>
2024-09-26 13:11:56 -0300

    medico controller

--- JavaMed/src/controller/MedicoController.java
+++ JavaMed/src/controller/MedicoController.java
@@ -1,14 +1,18 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import dao.ConsultaDAO;
 import dao.UsuarioDAO;
+import model.Consulta;
+import model.Medico;
 
 public class MedicoController extends UsuarioController {
 
     public MedicoController
-    (UsuarioDAO usuarioDAO) throws IOException {
-        super(usuarioDAO);
+    (UsuarioDAO usuarioDAO, ConsultaDAO consultaDAO) throws IOException {
+        super(usuarioDAO, consultaDAO);
     }
 
     /*  FUNÇOES A SEREM IMPLEMENTADAS
@@ -17,4 +21,27 @@ public class MedicoController extends UsuarioController {
      *        
     */
 
+    public void exibirConsultas(Medico medico) throws IOException {
+        List<Consulta> consultas;
+
+        int idMed = medico.getId();
+
+        consultas = consultaDAO.getConsultasByIdMedico(idMed);
+
+        for ( Consulta consulta : consultas ) {
+            System.out.println("Consulta dia " + consulta.getData());
+        }
+    }
+
+    public void visualizarDadosConsulta(Consulta consulta) throws IOException {
+        System.out.println("Dados da consulta");
+        System.out.println("Id: " + consulta.getIdConsulta());
+        System.out.println("Paciente: " + consulta.getPaciente());
+        System.out.println("Data e hora: " + consulta.getData());
+        System.out.println("Diagnostico: " + consulta.getDiagnostico());
+    }
+
+    public void registrarDiagnostico(Consulta consulta, String diagnostico) throws IOException {
+        consulta.setDiagnostico(diagnostico);
+    }
 }
