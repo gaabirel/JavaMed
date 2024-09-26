ea2eb8c2838ede3c5d7ca335d72814f37c0e06c9
Tiago Mamede <113864916+tiagomamede@users.noreply.github.com>
2024-09-26 14:13:05 -0300

    admin controller

--- JavaMed/src/controller/AdminController.java
+++ JavaMed/src/controller/AdminController.java
@@ -2,12 +2,21 @@ package controller;
 
 import java.io.IOException;
 
+import dao.AtendenteDAO;
+import dao.ConsultaDAO;
+import dao.MedicoDAO;
+import dao.PacienteDAO;
 import dao.UsuarioDAO;
+import model.Atendente;
+import model.Medico;
 
 public class AdminController extends AtendenteController {
+    AtendenteDAO atendenteDAO;
     
-    public AdminController(UsuarioDAO usuarioDAO) throws IOException {
-        super(usuarioDAO);
+    public AdminController(UsuarioDAO usuarioDAO, ConsultaDAO consultaDAO, PacienteDAO pacienteDAO, MedicoDAO medicoDAO, AtendenteDAO atendenteDAO) throws IOException {
+        super(usuarioDAO, consultaDAO, pacienteDAO, medicoDAO);
+
+        this.atendenteDAO = atendenteDAO;
     }
 
     @Override
@@ -21,5 +30,37 @@ public class AdminController extends AtendenteController {
      *      incluir, modificar e excluir os médicos e os atendentes
      * 
     */
+
+    public void incluirMedico(int id, String telefone, String cpf, String nome, String login, String senha) throws IOException {
+        Medico medico = new Medico(id, telefone, cpf, nome, login, senha);
+
+        medicoDAO.salvarUnico(medico);
+    }
+
+    public void modificarMedico(int idAntigo, int idNovo, String telefone, String cpf, String nome, String login, String senha) throws IOException {
+        Medico medico = new Medico(idNovo, telefone, cpf, nome, login, senha);
+
+        medicoDAO.modificar(idAntigo, medico);
+    }
+
+    public void excluirMedico(int idExc) throws IOException {
+        medicoDAO.excluirUnico(idExc);
+    }
+
+    public void incluirAtendente(int id, String telefone, String cpf, String nome, String login, String senha) throws IOException {
+        Atendente atendente = new Atendente(id, telefone, cpf, nome, login, senha);
+
+        atendenteDAO.salvarUnico(atendente);
+    }
+
+    public void modificarAtendente(int idAntigo, int idNovo, String telefone, String cpf, String nome, String login, String senha) throws IOException {
+        Atendente atendente = new Atendente(idNovo, telefone, cpf, nome, login, senha);
+
+        atendenteDAO.modificar(idAntigo, atendente);
+    }
+
+    public void excluirAtendente(int idExc) throws IOException {
+        atendenteDAO.excluirUnico(idExc);
+    }
 
 }
