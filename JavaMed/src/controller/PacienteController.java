e52eb05f0a09672ffa370489f86e83e61f4ce527
Tiago Mamede <113864916+tiagomamede@users.noreply.github.com>
2024-09-26 13:11:29 -0300

    paciente controller

--- JavaMed/src/controller/PacienteController.java
+++ JavaMed/src/controller/PacienteController.java
@@ -1,18 +1,44 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import dao.ConsultaDAO;
 import dao.UsuarioDAO;
+
+import model.Consulta;
+import model.Paciente;
 
 public class PacienteController extends UsuarioController {
 
-    public PacienteController(UsuarioDAO usuarioDAO) throws IOException {
-        super(usuarioDAO);
+    public PacienteController(UsuarioDAO usuarioDAO, ConsultaDAO consultaDAO) throws IOException {
+        super(usuarioDAO, consultaDAO);
+
     }
 
     /*  FUNÇOES A SEREM IMPLEMENTADAS
      *      ver consultas dele mesmo
-     *      
+     *      visualizar dados da consulta
      *  
     */
+
+    public void exibirConsultas(Paciente paciente) throws IOException {
+        List<Consulta> consultas;
+
+        int idPac = paciente.getId();
+
+        consultas = consultaDAO.getConsultasByIdPaciente(idPac);
+
+        for ( Consulta consulta : consultas ) {
+            System.out.println("Consulta dia " + consulta.getData());
+        }
+    }
+
+    public void visualizarDadosConsulta(Consulta consulta) throws IOException {
+        System.out.println("Dados da consulta");
+        System.out.println("Id: " + consulta.getIdConsulta());
+        System.out.println("Paciente: " + consulta.getPaciente());
+        System.out.println("Medico: " + consulta.getPaciente());
+        System.out.println("Data e hora: " + consulta.getData());
+    }
 }
